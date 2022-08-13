e23fb7b1c57ec460bffce8af31db15f1640bab04
Iarinaly <rlanjaiarinaly@gmail.com>
2022-08-13 15:17:45 +0300

    Final Commit

--- 1.py
+++ 1.py
@@ -1,16 +1,20 @@
 from collections import defaultdict
+import re
 def isValidHtml(html) :
     d = defaultdict(int)
-    html =html.split('<')
-    for a in html :
+    htmlt =html.split('<')
+    for a in htmlt :
         if ">" in a and "/" not in a :
             d[a[:len(a)-1]] += 1
         elif ">" in a and "/" in a :
             d[a[1:len(a)-1]] -= 1
             if d[a[1:len(a)-1]] < 0 :
                 return False
-            
+
     for i,v in d.items():
         if v != 0 :
             return False
-    return True 
+    r = re.search("<([a-z]+)>*<([a-z]+)>*</([a-z]+)>*</([a-z]+)>",html)
+    if r is not None and (r.group(2) != r.group(3)) :
+        return False
+    return True
