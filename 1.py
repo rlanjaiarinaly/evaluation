3ced59573a0707f10f016e3c2f89fa17c22058af
Iarinaly <rlanjaiarinaly@gmail.com>
2022-08-13 15:30:50 +0300

    Final Commit

--- 1.py
+++ 1.py
@@ -8,8 +8,8 @@ def isValidHtml(html) :
         if ">" in a and "/" not in a :
             d[a[:a.index(">")]] += 1
         elif ">" in a and "/" in a :
-            d[a[1:len(a)-1]] -= 1
-            if d[a[1:len(a)-1]] < 0 :
+            d[a[1:a.index(">")]] -= 1
+            if d[a[1:a.index(">")]] < 0 :
                 return False
 
     for i,v in d.items():
